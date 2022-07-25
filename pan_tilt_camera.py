9f90cf7bbed8226281d461c2b91e88e48fbd0945
AndyKong2020 <andy.kong@nuaa.edu.cn>
2022-07-25 18:28:03 +0800

    [feat]openmv控制云台

---
+++ pan_tilt_camera.py
@@ -0,0 +1,53 @@
+import sensor, image, time
+
+from pid import PID
+from pyb import Servo
+
+pan_servo=Servo(2)
+tilt_servo=Servo(3)
+
+pan_servo.calibration(500,2500,1500)
+tilt_servo.calibration(500,2500,1500)
+
+red_threshold  = (11, 53, 28, 95, 6, 76)
+
+pan_pid = PID(p=0.07, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
+tilt_pid = PID(p=0.05, i=0, imax=90) #脱机运行或者禁用图像传输，使用这个PID
+#pan_pid = PID(p=0.1, i=0, imax=90)#在线调试使用这个PID
+#tilt_pid = PID(p=0.1, i=0, imax=90)#在线调试使用这个PID
+
+sensor.reset() # Initialize the camera sensor.
+sensor.set_pixformat(sensor.RGB565) # use RGB565.
+sensor.set_framesize(sensor.QVGA) # use QQVGA for speed.
+sensor.skip_frames(10) # Let new settings take affect.
+sensor.set_auto_whitebal(False) # turn this off.
+clock = time.clock() # Tracks FPS.
+
+def find_max(blobs):
+    max_size=0
+    for blob in blobs:
+        if blob[2]*blob[3] > max_size:
+            max_blob=blob
+            max_size = blob[2]*blob[3]
+    return max_blob
+
+
+while(True):
+    clock.tick() # Track elapsed milliseconds between snapshots().
+    img = sensor.snapshot() # Take a picture and return the image.
+
+    blobs = img.find_blobs([red_threshold], pixels_threshold=100)
+    if blobs:
+        max_blob = find_max(blobs)
+        pan_error = max_blob.cx()-img.width()/2
+        tilt_error = max_blob.cy()-img.height()/2
+
+
+        img.draw_rectangle(max_blob.rect()) # rect
+        img.draw_cross(max_blob.cx(), max_blob.cy()) # cx, cy
+
+        pan_output=pan_pid.get_pid(pan_error,1)/2
+        tilt_output=tilt_pid.get_pid(tilt_error,1)
+        print(pan_servo.angle(),tilt_servo.angle())
+        pan_servo.angle(pan_servo.angle()+pan_output)
+        tilt_servo.angle(tilt_servo.angle()-tilt_output)
